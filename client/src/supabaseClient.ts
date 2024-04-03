85698cfecd72eb63abea05d85b42f98bcf0cd0b5
magdalsen <magdal.sen@gmail.com>
2024-04-03 11:55:40 +0200

    add server and client folders

---
+++ client/src/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_URL
+const supabaseKey = import.meta.env.VITE_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
